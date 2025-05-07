c01280bb712d5e79ac7486568449a5c71da7e97f
Radwan Mansur <raydiaz1899@gmail.com>
2025-05-07 09:21:41 +0300

    style

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -1,9 +1,29 @@
-
+import job1 from '../assets/job1.jpg'
 
 function Login() {
     return (
-        <div>
-            2
+        <div className='login-signup'>
+            <h1>login</h1>
+            <form >
+                <div className='form-group'>
+                    <input type="text" placeholder='email' />
+                </div>
+                <div className='form-group'>
+                    <input type="password" placeholder='password' />
+
+                </div>
+                <div className='form-group'>
+                    <button>submit</button>
+                </div>
+                <section className='image-container'>
+                    <img src={job1} alt="alt" />
+                    <img src={job1} alt="alt" />
+                    <img src={job1} alt="alt" />
+                </section>
+            </form>
+
+
+
         </div>
     )
 }
