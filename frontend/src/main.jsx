f30c6fbac217b364f0ed2d0940405796fb5b0df8
Radwan Mansur <raydiaz1899@gmail.com>
2025-05-07 08:05:35 +0300

    first

---
+++ frontend/src/main.jsx
@@ -0,0 +1,14 @@
+import { createRoot } from 'react-dom/client'
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+import App from './App.jsx'
+
+import './styles/index.scss'
+
+
+createRoot(document.getElementById('root')).render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
+
+)
