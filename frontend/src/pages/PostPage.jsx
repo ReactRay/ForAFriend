0f2ca56601467d2cb872dd42ec0d108ed05b0b95
Radwan Mansur <raydiaz1899@gmail.com>
2025-05-08 20:38:01 +0300

    styles

--- frontend/src/pages/PostPage.jsx
+++ frontend/src/pages/PostPage.jsx
@@ -26,7 +26,7 @@ function PostPage() {
                     <p className="contact">Contact: {currentPage.contact}</p>
 
                     <div className="user-info">
-                        <img className="avatar" src={currentPage.user.profilePic} alt={currentPage.user.fullName} />
+                        <img className="avatar" src={currentPage.user?.profilePic} alt={currentPage.user.fullName} />
                         <span className="username">{currentPage.user.fullName}</span>
                         <p className="username">{currentPage.user.email} </p>
                     </div>
